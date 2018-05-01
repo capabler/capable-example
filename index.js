e0f22172ba70c2b92261c35db351ed46e9c90630
topthinking <topthinking@ximalaya.com>
2018-05-01 09:53:14 +0800

    入口

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+/**
+ * capable框架入口文件，提供环境变量的初始化等相关事情
+ * 不做任何方法的执行，保证只有一个core方法的执行
+ * 让入口看上去更简洁，更明了
+ * 同时入口文件一般也是不需要去进行改变的
+ */
 const path = require('path')
+
 /**
  * 系统环境参数的配置
  */
 global.application = path.join(__dirname, 'application')
-
-/**
- * 如果选择react模板，需要添加该配置，否则无法进行模板的解析
- */
-require('babel-register')({
-  presets: [ "es2015", "react"],
-  extensions: [ '.jsx' ],
-});
 
 //启动核心方法
 require('./system/core')()
